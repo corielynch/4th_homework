e59e299d73214ce488ac710cf1f28440e43237a1
Corie Lynch <Lynchc17@gmail.com>
2020-04-22 11:20:35 -0700

    Added 5 quiz questions

--- app.js
+++ app.js
@@ -4,8 +4,23 @@ var timer = setInterval(function() {
 })
 
 //Confirm for user to begin quiz
-var confirmQuiz = confirm("30 Second JavaScript quiz. Are you ready??");
-    if (confirm) {
-    } else {
-        alert ("Ok we won't begin yet.");
-    }
+
+
+//5 multiple choice questions
+var question1 = ("Which of these is NOT a JavaScript data type?");
+var answer1Choices = ["String", "Boolean", "Number", "Method"];
+var answer1 = ("Method");
+var question2 = ("Which company first developed JavaScript?");
+var answer2Choices = ["Macintosh", "Netscape", "Microsoft", "IBM"];
+var answer2 = ("Netscape");
+var question3 = ("How long did it take Brendan Eich to create JavaScript?");
+var answer3Choices = ["2 weeks", "3 days", "10 hours", "5 months"];
+var answer3 = ("10 hours");
+var question4 = ("document.getElementById() is an example of what?");
+var answer4Choices = ["For loops", "While loops", "DOM manipulation", "pseudocode"];
+var answer4 = ("DOM manipulation");
+var question5 = ("Which symbol links ID's from HTML to CSS?");
+var question5Choices = ["@", "#", "." "{ }"];
+
+
+//eventListener for completion of question 5 or 30 seconds.
